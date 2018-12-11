a8a6ca6c422b74754e7426e240d1e7e58323680f
chenfangzhi <1242692800@qq.com>
2018-12-11 14:11:11 +0800

    init

--- redis_pro.py
+++ redis_pro.py
@@ -1,15 +1,36 @@
 import redis
+import time
+import pprint
 
 if __name__ == '__main__':
-    # decode_responses表名使用str而不是bytes类型
+    start = time.time()
+    # # decode_responses表名使用str而不是bytes类型
     r = redis.StrictRedis(host='10.10.32.167', port=16379, db=0, decode_responses=True)  # 显示指定主机、端口号和数据库
-    members = r.smembers("robotchat:block:list:")
-    get_str = r.get('relation.push.config')
+    # 采用管道版本
+    # with r.pipeline(transaction=False) as p:
+    #     for line in open('unread.txt'):
+    #         key = line.strip()
+    #         p.hdel('robotchat:unread:count:', key)
+    #         p.zrem("robotchat:allocated:", key)
+    #         p.sadd("robotchat:block:list:", key)
+    #     execute = p.execute()
+    #     pprint.pprint(execute)
+    # print("总操作时间：", time.time() - start)
 
-    # print(get_str)
-    for i in members:
-        split = str(i).strip("\"").split("_")
-        print('UPDATE `customer_relation` SET is_blocked=1 WHERE user_id=' + split[0] + ' and robot_id=' + split[1]+';')
+    # 采用多命令版本
+    my_list = list(map(lambda x: x.strip(), open('unread.txt')))
+    print(r.hdel('robotchat:unread:count:', *my_list))
+    print(r.zrem("robotchat:allocated:", *my_list))
+    print(r.sadd("robotchat:block:list:", *my_list))
+
+    # members = r.smembers("robotchat:block:list:")
+    # get_str = r.get('relation.push.config')
+    #
+    # # print(get_str)
+    # for i in members:
+    #     split = str(i).strip("\"").split("_")
+    #     print(
+    #         'UPDATE `customer_relation` SET is_blocked=1 WHERE user_id=' + split[0] + ' and robot_id=' + split[1] + ';')
 
     # s = b'hello\n'
     # s2 = 'hello\n'
